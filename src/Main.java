508bc8d261cdac0905b6da93c0f42e7a3b8cf1c6
LeeJaeHyeok97 <hazardous10@naver.com>
2025-03-30 15:27:51 +0900

    docs: 구현할 데이터베이스 기능 목록 작성

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    private final String filePath;
+    private final Map<String, Long> indexMap = new HashMap<>();
+
+    public Main(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
